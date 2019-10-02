5b3160074db52b706f68ca2f55b3554920eb83ab
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-02 15:09:33 -0500

    room and player set up

--- src/player.py
+++ src/player.py
@@ -1,2 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name: name
+        self.current_room: current_room
+
+    def __str__(self):
+        return(f'{self.name} is in {self.current_room}')
+
+    def move(self, direction):
+        if direction == 'n':
+            self.current_room = self.current_room.n_to
+        elif direction == 'e':
+            self.current_room = self.current_room.e_to
+        elif direction == 'w':
+            self.current_room = self.current_room.w_to
+        elif direction == 's':
+            self.current_room = self.current_room.s_to
