5fa52b1156cb3a192b23f28a88da38ca553774d6
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-02 16:10:41 -0500

    set up with player, room, item

--- src/player.py
+++ src/player.py
@@ -3,9 +3,10 @@
 
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items):
         self.name: name
         self.current_room: current_room
+        self.items = []
 
     def __str__(self):
         return(f'{self.name} is in {self.current_room}')
